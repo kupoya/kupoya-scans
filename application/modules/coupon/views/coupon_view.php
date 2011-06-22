bd1a3c102df283620fe2eeb30ef6b744c3c630ba
Liran Tal <liran.tal@gmail.com>
2011-06-22 22:28:48 +0300

    fixed site_url() function typo

--- application/modules/coupon/views/coupon_view.php
+++ application/modules/coupon/views/coupon_view.php
@@ -50,7 +50,7 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 	<div id="header">
 		
 		<div id="logo">
-			<img src='<?=$site_url($picture)?>' alt='logo' width='' height='<?=$picture_height?>' />
+			<img src='<?=site_url($picture)?>' alt='logo' width='' height='<?=$picture_height?>' />
 		</div>
 
 		<div id="breadcrumb_happy">
