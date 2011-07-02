f77080304bfd826e380b7965645b92170a9a6f1c
Liran Tal <liran.tal@gmail.com>
2011-07-02 16:07:38 +0300

    updating error templates to have the kupoya look and feel

--- application/errors/error_db.php
+++ application/errors/error_db.php
@@ -1,34 +1,54 @@
-<html>
-<head>
-<title>Database Error</title>
-<style type="text/css">
-
-body {
-background-color:	#fff;
-margin:				40px;
-font-family:		Lucida Grande, Verdana, Sans-serif;
-font-size:			12px;
-color:				#000;
-}
-
-#content  {
-border:				#999 1px solid;
-background-color:	#fff;
-padding:			20px 20px 12px 20px;
-}
-
-h1 {
-font-weight:		normal;
-font-size:			14px;
-color:				#990000;
-margin:				0 0 4px 0;
-}
-</style>
-</head>
-<body>
-	<div id="content">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
-</body>
-</html>
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head> 
+	<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" /> 
+	<meta http-equiv="cache-control" content="max-age=200" /> 
+	<title>Error</title> 
+			
+	<!-- CSS base --> 
+	<link href="/themes/mobile_v1/views/assets/css/style.css" type="text/css" media="handheld, screen" rel="stylesheet" /> 
+ 
+	<!-- JavaScript base 
+		--> 
+</head> 
+<body> 
+<div class="mainwrapper"> 
+<div class="top-bg"> 
+ 
+	<div id="header"> 
+		
+		<div id="logo"> 
+			<img src="/themes/mobile_v1/views/assets/img/kupoya.png" width="120" height="31" alt="kupoya" /> 
+		</div> 
+ 
+		<div id="breadcrumb"> 
+			<h3 id='white'> sorry, we couldn't process your request </h3> 
+		</div> 
+		
+	</div> 
+ 
+ 
+	<div id="content"> 
+ 
+		<div class="min-width"> 
+		<div id="center"> 
+			try again later
+			<?php //echo $message; ?>
+			<br/> 
+			<img src="/themes/mobile_v1/views/assets/img/kupi_sad_small.png" alt="kupi is sad" width="123" height="132" /> 
+			<br/><br/><br/> 
+		</div> 
+		</div> 
+ 
+	</div> 
+	
+	<div id="footer"> 
+					powered by
+			<img src="/themes/mobile_v1/views/assets/img/kupoya.png" width="120" height="31" alt="kupoya" /> 
+			<p>kupoya &nbsp;&copy;&nbsp; 2011 <br /><a href="/info/privacy_policy">privacy policy</a></p> 
+ 
+	</div> 
+</div> 
+</div> 
+</body> 
+</html> 
