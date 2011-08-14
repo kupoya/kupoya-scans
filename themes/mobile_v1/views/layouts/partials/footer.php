e8e97500264de95fb7587e539295fb9bfe7f8b3c
Liran Tal <liran.tal@gmail.com>
2011-08-14 23:08:33 +0300

    added terms of use link

--- themes/mobile_v1/views/layouts/partials/footer.php
+++ themes/mobile_v1/views/layouts/partials/footer.php
@@ -1,4 +1,10 @@
 			powered by
 			<?= image('kupoya.png', '_theme_', array('width'=>'120', 'height'=>'31')); ?>
-			<p>kupoya &nbsp;&copy;&nbsp; 2011 <br /><a href="/info/privacy_policy">privacy policy</a></p>
+			<p>
+				kupoya &nbsp;&copy;&nbsp; 2011
+				<br/>
+				<a href="/info/privacy_policy">privacy policy</a>
+				|
+				<a href="/info/terms_of_use">terms of use</a>
+			</p>
 
