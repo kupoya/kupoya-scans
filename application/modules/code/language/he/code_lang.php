4164ad438d9ce055ef6035946e38e1aec3a280d5
Liran Tal <liran.tal@gmail.com>
2011-09-11 00:59:36 +0300

    minor language update

--- application/modules/code/language/he/code_lang.php
+++ application/modules/code/language/he/code_lang.php
@@ -6,7 +6,7 @@
 // code/login.php
 $lang['Login_to_your_facebook_account'] = 'התחבר לחשבון הפייסבוק שלך';
 $lang['Step_1/2'] = 'צעד 1/2';
-$lang['facebook_connect'] = 'חיבור לפייסבוק';
+$lang['facebook_connect'] = 'התחבר עם פייסבוק';
 
 // code/code_invalid.php
 $lang['Invalid_code'] = 'קוד לא חוקי';
