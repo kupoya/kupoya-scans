052e8d054b0ad04de310996d20ff862b4f908e62
Liran Tal <liran.tal@gmail.com>
2011-11-26 13:47:35 +0200

    more fixes (removed commas per request in https://kupoya.podio.com/kupoyascans/item/463804) to random wallpost pool

--- application/language/he/app_lang.php
+++ application/language/he/app_lang.php
@@ -12,7 +12,7 @@ $lang['message_generic_post'] = array(
 	// [brand_name] [brand_address] [strategy_name]
 	"וואו ל%s %s יש מבצע מוגבל, הרגע קיבלתי %s",
 	// [brand_name] [brand_address] [strategy_name]
-	"חברים, תקפצו ל%s %s - הם נותנים %s לכל מי שסורק את הקוד שלהם, אני כבר קיבלתי :-)",
+	"חברים תקפצו ל%s %s - הם נותנים %s לכל מי שסורק את הקוד שלהם, אני כבר קיבלתי :-)",
 	// [brand_name] [brand_address] [strategy_name]
 	"וואו! %s %s נתנו לי %s. מגניב הא?!",
 	// [brand_name] [brand_address] [strategy_name]
