ade0d02234e598f9189200a94582d886a8dbc1fa
Liran Tal <liran.tal@gmail.com>
2012-03-04 23:30:55 +0200

    in session with ram, deciding to switch from brand logo to plain-text only for kupoya signature | also updated copyright to 2012

--- themes/mobile_v1/views/layouts/partials/footer.php
+++ themes/mobile_v1/views/layouts/partials/footer.php
@@ -1,7 +1,7 @@
-			powered by
-			<?= image('kupoya.png', '_theme_', array('width'=>'120', 'height'=>'31')); ?>
+			
+			<?php //echo image('kupoya.png', '_theme_', array('width'=>'120', 'height'=>'31')); ?>
 			<p>
-				kupoya &nbsp;&copy;&nbsp; 2011
+				powered by kupoya &nbsp;&copy;&nbsp; 2012
 				<br/>
 				<a href="/info/privacy_policy">privacy policy</a>
 				|
