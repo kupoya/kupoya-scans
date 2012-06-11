cba0bd1363a5a77e1d2540b5589f1d161125f958
Liran Tal <liran.tal@gmail.com>
2012-06-11 23:26:48 +0300

    updating info page css

--- application/views/info.php
+++ application/views/info.php
@@ -3,7 +3,7 @@
 
 		<ul data-role="listview" data-split-icon="arrow-r" data-split-theme="d" class="ui-listview">
             <li data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="false" data-iconpos="right" data-theme="c" >
-               <a href="http://www.kupoya.com"><img src="<?= image_path('icon/286-speechbubble@2x.png', '_theme_'); ?>" class="ui-li-thumb">
+               <a href="http://www.kupoya.com"><img src="<?= image_path('icon/286-speechbubble@2x.png', '_theme_'); ?>" class="ui-li-thumb-center">
 				<h3 class="ui-li-heading">
 					<?= $this->lang->line('About');?>
 				</h3>
@@ -12,7 +12,7 @@
                 <a href="#"></a>
             </li>
                         <li data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="false" data-iconpos="right" data-theme="c" >
-               <a href="<?= site_url('/info/privacy_policy'); ?>"><img src="<?= image_path('icon/30-key@2x.png', '_theme_'); ?>" class="ui-li-thumb">
+               <a href="<?= site_url('/info/privacy_policy'); ?>"><img src="<?= image_path('icon/30-key@2x.png', '_theme_'); ?>" class="ui-li-thumb-center">
 				<h3 class="ui-li-heading">
 					<?= $this->lang->line('Privacy_Policy');?>
 				</h3>
@@ -21,7 +21,7 @@
                 <a href="#"></a>
             </li>
                         <li data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="false" data-iconpos="right" data-theme="c" >
-               <a href="<?= site_url('/info/terms_of_use'); ?>"><img src="<?= image_path('icon/96-book@2x.png', '_theme_'); ?>" class="ui-li-thumb">
+               <a href="<?= site_url('/info/terms_of_use'); ?>"><img src="<?= image_path('icon/96-book@2x.png', '_theme_'); ?>" class="ui-li-thumb-center">
 				<h3 class="ui-li-heading">
 					<?= $this->lang->line('Terms_Of_Use');?>
 				</h3>
